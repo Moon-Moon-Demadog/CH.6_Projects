a52795d427aba2c0f73e403327a521ad21b70d17
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-19 18:54:08 +0000

    adding headers

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,1 +1,15 @@
 // This Program will calculate safest driving area in a major city.
+#include <iostream>
+#include <string>
+using namespace std;
+
+string getRegInfo (string);
+int getRegInfo (int);
+bool isLower (int);
+string showLowest (string);
+int showLowest (int);
+
+int main()
+{
+  
+}
