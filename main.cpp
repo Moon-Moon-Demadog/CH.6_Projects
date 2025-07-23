7ef453ced4a98b8b131dc64f69a52024baf1231a
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-23 17:50:08 +0000

    initial set up

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-// This Program will calculate safest driving area in a major city.
+// This Program will calculate an estimate for a paint job.
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int getRoom (int rooms, int sqft);
+double getPaint (int, int paint);
+double CalcLabor (double LaborHours, double LaborPay);
+void DisplayTotals ();
+
+static double gallonsPerSqft = 1/110;
+static double laborPerSqft = 8/110;
+static double LaborPerHour = 25.00;
+
+int main ()
+{
+  
+}
